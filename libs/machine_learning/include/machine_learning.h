56e748134f96053305dd4f9170b97c9dd58f1cf1
kamilus1 <kbouhaouli@gmail.com>
2025-07-17 22:45:04 +0200

    Added basic saving/reading of ML song parameters

--- libs/machine_learning/include/machine_learning.h
+++ libs/machine_learning/include/machine_learning.h
@@ -2,6 +2,7 @@
 #define _MACHINE_LEARNING_H_
 #include <stdbool.h>
 #include <stdint.h>
+#include <json-glib/json-glib.h>
 
 typedef enum _MusicGenre{
     NO_GENRE = 0,
@@ -25,5 +26,20 @@ typedef struct _MLNightcoreData
     float reverb_value;
 
 }MLNightcoreData;
+
+int ml_nightcore_data_init(MLNightcoreData *ml_nightcore_data, float origin_bpm, float output_bpm, 
+                            float pitch, float output_speed, float input_bass, float output_bass, 
+                            float bass_boost, bool is_reverb, 
+                            uint32_t reverb_delay, float reverb_feedback, float reverb_value);
+
+gchar* ml_nightcore_data_to_json(MLNightcoreData *ml_nightcore_data);
+
+int ml_nightcore_data_from_json(MLNightcoreData *ml_nightcore_data, const char *json_buffer);
+
+int ml_nightcore_data_free(MLNightcoreData *ml_nightcore_data);
+
+int ml_nightcore_data_json_to_file(MLNightcoreData *ml_nightcore_data, const char *file_path);
+
+
 
 #endif
